26384b03e73b20726d291af903236990ce786af8
Suraj <wijewickramasuraj@gmail.com>
2023-01-25 22:04:43 +0530

    suraj commit on line 4

--- client/ANIMUS/src/App.jsx
+++ client/ANIMUS/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function App(props) {
-  return <div>ANIMUS CLient Suraj commit</div>;
+  return <div>ANIMUS CLient Suraj commit ssidasda</div>;
 }
 
 export default App;
