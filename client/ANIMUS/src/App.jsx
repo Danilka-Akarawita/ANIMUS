559817795014790b1b8ef2a00dbe1531f6899a6c
Suraj <wijewickramasuraj@gmail.com>
2023-01-27 23:03:01 +0530

    Init Home

--- client/ANIMUS/src/App.jsx
+++ client/ANIMUS/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Container from "./components/Container";
+import Home from "./home-page/Home";
 function App(props) {
   return (
     <div>
       ANIMUS CLient Suraj commit ssidasda
       <div>new div</div>
       <Container />
+      <Home />
     </div>
   );
 }
