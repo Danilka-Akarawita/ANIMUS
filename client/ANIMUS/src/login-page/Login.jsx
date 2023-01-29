082f64d179e7b918c7e9a4e01b0421a369c2b191
Suraj <wijewickramasuraj@gmail.com>
2023-01-29 23:47:20 +0530

    Refactoring - Suraj

--- client/ANIMUS/src/login-page/Login.jsx
+++ client/ANIMUS/src/login-page/Login.jsx
@@ -1,34 +1,16 @@
-import React from 'react'
-import AnimusTittle from '../components/AnimusTittle'
-import LoginButton from '../components/loginButton'
-import Password from '../components/Password'
-import UserName from '../components/UserName'
-
-
-
+import React from "react";
+import AnimusTittle from "../components/AnimusTittle";
+import LoginButton from "../components/loginButton";
+import Password from "../components/Password";
+import InputField from "../components/InputField";
 
 export default function Login() {
   return (
-  <div className='loginBox'>
-    <AnimusTittle/>
-    
-    <UserName/>
-    <Password/>
-    <LoginButton/>
-    
-    
-
-
-    
-    
-   
-
-    
-   
+    <div className="loginBox">
+      <h1> ANIMUS</h1>
+      <InputField name={"UserName"} placeholder="UserName/Email Address" />
+      <InputField name={"Password"} placeholder="Password" />
+      <LoginButton />
     </div>
-
-      
-    
-  )
+  );
 }
-
