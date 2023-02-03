4e7ea43da8d9baa6d6bfa28eee89c4292451456d
Danilka-Akarawita <danilka.20210514@iit.ac.lk>
2023-02-03 14:54:06 +0530

    sign-in-auth

--- client/ANIMUS/firebase/autenticate.jsx
+++ client/ANIMUS/firebase/autenticate.jsx
@@ -15,3 +15,28 @@ export const signUpUser = async (email, password) => {
     console.log(error.message);
   }
 };
+
+
+export const signInUser = async (email, password) => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
+    }
+    
+    
+    export const signOutUser = async (email, password) => {
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+      })
+      .catch((error) => {
+        // An error happened.
+      });
+    }
