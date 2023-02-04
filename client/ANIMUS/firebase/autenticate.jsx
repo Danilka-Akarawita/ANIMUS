ac221372cbf73a7557fa3de5f67371362a113ff2
Suraj Wijewickrama <90107939+SurajWijewickrama@users.noreply.github.com>
2023-02-04 14:38:20 +0530

    chore: clean up home , loggin , signup

--- client/ANIMUS/firebase/autenticate.jsx
+++ client/ANIMUS/firebase/autenticate.jsx
@@ -3,10 +3,13 @@ import {
   getAuth,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
-  signInWithPopup, GoogleAuthProvider, signOut
+  signInWithPopup,
+  GoogleAuthProvider,
+  signOut,
 } from "firebase/auth";
 
 const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
 
 export const signUpUser = async (email, password) => {
   try {
@@ -17,32 +20,27 @@ export const signUpUser = async (email, password) => {
   }
 };
 
-
-
 export const signInUser = async (email, password) => {
-  try{
-  const user=await signInWithEmailAndPassword(auth, email, password)
-    console.log(user)
-    }catch (error) {
-      console.log(error.message);
-    }
-  };
-
-
-  export const signOutUser = async (email, password) => {
-  try{
-    await signOut(auth)
-  }catch(error){
-    console.log(error.message)
+  try {
+    const user = await signInWithEmailAndPassword(auth, email, password);
+    console.log(user);
+  } catch (error) {
+    console.log(error.message);
   }
-}
+};
+
+export const signOutUser = async (email, password) => {
+  try {
+    await signOut(auth);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
 export const googleSignIn = async () => {
-  try{
-     const user= await signInWithPopup(auth, provider)
-   
-     }catch(error){
-         console.log(error.message)
-         
-     };
+  try {
+    const user = await signInWithPopup(auth, provider);
+  } catch (error) {
+    console.log(error.message);
   }
+};
