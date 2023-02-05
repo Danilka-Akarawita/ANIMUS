ce5144b5486931c3df867b9c55aa89759f3af73b
ViharaJayaweera <103949800+ViharaJayaweera@users.noreply.github.com>
2023-02-05 22:36:58 +0530

    feat:implemented points and aboutus-Vihara

---
+++ client/ANIMUS/src/aboutus-page/AboutUs.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./AboutUs.css";
+
+export default function AboutUs() {
+    return (
+  
+        <div>
+        <h1>About Us</h1>
+        <br/>
+        <p>
+            <ol>
+            <li>Suraj Wijewickrama  &nbsp;&nbsp;&nbsp;&nbsp;  w1867065   &nbsp;&nbsp;  20210294</li>
+            <li>Danilka Akarawita &nbsp;&nbsp;&nbsp;&nbsp; w1867644 &nbsp;&nbsp; 20210514</li>
+            <li>Vihara Jayaweera &nbsp;&nbsp;&nbsp;&nbsp; w1833638 &nbsp;&nbsp; 20200480 </li>
+            <li>Chethiya Wijerathne &nbsp;&nbsp;&nbsp;&nbsp; w1914622 &nbsp;&nbsp; 20211136 </li>
+            <li>M.S. Umair &nbsp;&nbsp;&nbsp; w1912936 &nbsp;&nbsp; 20210209 </li>
+            </ol>
+
+</p>
+      </div>
+    );
+};
---
+++ client/ANIMUS/src/point-page/Point.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import "./Point.css";
+
+
+
+export default function Point() {
+    return (
+  
+      <div>
+        <h1>How to get points</h1>
+        <br/>
+        <p>1 point<br/> 
+        <ul>
+            <li>Take your own sustainable bag (1 point per bag) </li>
+            <li>Refuse to use the polythene bag provided</li>
+        </ul><br/>
+
+        The user can earn points by taking own sustainable bags for their<br/>
+        grocery shopping.They can earn one point per each bag.Also when<br/>
+        the shopper refuse to use the polythene bag when it's not required<br/>
+         they will get point. 
+        </p>
+      </div>
+      
+    )
+  }
+  
