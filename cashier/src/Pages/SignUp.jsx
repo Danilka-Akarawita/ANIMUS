af202a5247d6e1b23dbf6494d0b3884b50df5c61
umairShukri <umair.20210209@iit.ac.lk>
2023-02-07 09:50:15 +0530

    cashierSignuppage

--- cashier/src/Pages/SignUp.jsx
+++ cashier/src/Pages/SignUp.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import  Logo from "../assets/main-logo.svg";
 
 export default function SignUp() {
   return (
     <div>
-        <div>
-            <div className='flex flex-row'> </div>
-
+        <div className="flex flex-col space-y-6 border-4 rounded-lg border-yellow-100  justify-center bg-teal-900 p-4 flex items-center  ">
+        <img className="" src={Logo} alt="logo"/>
+            <div className="flex flex-row space-x-6 "> 
+            <input type="text" placeholder="First name" className="p-2 "
+          name="firstname"/>
+          <input type="text" placeholder="Surname" className="p-2 "
+          name="surname"/>
+          </div>
+          <input type="text" placeholder='Cashier Id' className="p-2  " name='cashierId'/>
+          <input type="text" placeholder='Username / email address' className="p-2  " name='username/email'/>
+          <input type="text" placeholder='New password' className="p-2  " name='newPassword'/>
+          <button className="text-white  hover:bg-black"  > SIGN UP</button>
         </div>
       
     </div>
