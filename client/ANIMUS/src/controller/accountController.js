12ccf9e838638d54d04da08c27f842a716ac3ce3
Suraj Wijewickrama <90107939+SurajWijewickrama@users.noreply.github.com>
2023-02-26 21:51:41 +0530

    api connected to client

--- client/ANIMUS/src/controller/accountController.js
+++ client/ANIMUS/src/controller/accountController.js
@@ -3,7 +3,7 @@ import { json } from "react-router-dom";
 export const getUserFromUID = async (uid) => {
   console.log(uid);
   const response = await fetch(
-    `https://animus-production.up.railway.app//api/accounts/${uid}`
+    `https://animus-production.up.railway.app/api/accounts/${uid}`
   );
   const user = await response.json();
   console.log(user);
@@ -14,7 +14,7 @@ export const createUserFromUID = async (name, mobile, uid) => {
   const data = { name, mobile, UID: uid };
   console.log(data);
   const response = await fetch(
-    `https://animus-production.up.railway.app//api/accounts/`,
+    `https://animus-production.up.railway.app/api/accounts/`,
     {
       method: "POST",
       body: JSON.stringify(data),
