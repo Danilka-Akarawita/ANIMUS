3a59a07a74684ea48c3f5cc307e307aa9db87848
Suraj Wijewickrama <90107939+SurajWijewickrama@users.noreply.github.com>
2023-03-08 09:46:46 +0530

    debug -2

--- Server/Controllers/accountControllers.js
+++ Server/Controllers/accountControllers.js
@@ -39,7 +39,8 @@ const createNewAccount = async (req, res) => {
 const updateAccount = async (req, res) => {
   const { uid } = req.params;
   console.log("loading account");
-  console.log(req.body);
+  console.log({ ...req.body });
+  console.log({ UID: uid });
   const account = await Account.findOneAndUpdate({ UID: uid }, { ...req.body });
   if (!account) return res.status(400).json({ error: "No account found" });
 };
